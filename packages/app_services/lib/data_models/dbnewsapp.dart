791bd79ad94ac050aa284253793da7c344b8a715
askhatzhax <ash.zhaxybaev001@gmail.com>
2022-08-07 10:16:09 +0600

    добавил описание

---
+++ packages/app_services/lib/data_models/dbnewsapp.dart
@@ -0,0 +1,11 @@
+class DBApp{
+  int id;
+  String imgUrl;
+  String newsTitle;
+  String fullText;
+  String author;
+  String date;
+  String originUrl;
+  String category;
+  DBApp(this.id=0,this.newsTitle='',this.)
+}
